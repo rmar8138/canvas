49c70b58704f054e078bd0b1a30c9e7307c049a4
rmar8138 <ragan.martinez@live.com>
2020-01-07 10:27:51 +1100

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to your drawing pad</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
